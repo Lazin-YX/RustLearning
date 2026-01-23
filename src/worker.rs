aa9ec389009e1e22ae9f3b0cb31dc5dba9ab438b
Yixuan Zhou <yixuanzhou@lazin.jp>
2026-01-23 16:18:34 +0800

    the rust programming language finished

---
+++ src/worker.rs
@@ -0,0 +1,36 @@
+use std::sync::{mpsc, Arc, Mutex};
+use std::thread;
+use crate::Message;
+
+pub(crate) struct Worker {
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>,
+}
+
+impl Worker {
+    pub(crate) fn id(&self) -> usize {
+        self.id
+    }
+
+    pub(crate) fn thread(&mut self) -> &mut Option<thread::JoinHandle<()>> {
+        &mut self.thread
+    }
+    pub(crate) fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let message = receiver.lock().unwrap().recv().unwrap();
+
+            match message {
+                Message::NewJob(job) => {
+                    println!("Worker {} got a job; executing.", id);
+                    job();
+                },
+                Message::Terminate => {
+                    println!("Worker {} was told to terminate.", id);
+                    break;
+                }
+            }
+        });
+
+        Worker { id, thread: Some(thread) }
+    }
+}
