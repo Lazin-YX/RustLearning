5b0edbe25abda3f0d18332bac80f0f9e25cb5269
Yixuan Zhou <yixuanzhou@lazin.jp>
2026-01-26 17:31:38 +0800

    type transfer

--- src/worker.rs
+++ src/worker.rs
@@ -1,6 +1,6 @@
-use std::sync::{mpsc, Arc, Mutex};
-use std::thread;
 use crate::Message;
+use std::sync::{Arc, Mutex, mpsc};
+use std::thread;
 
 pub(crate) struct Worker {
     id: usize,
@@ -16,21 +16,26 @@ impl Worker {
         &mut self.thread
     }
     pub(crate) fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
-        let thread = thread::spawn(move || loop {
-            let message = receiver.lock().unwrap().recv().unwrap();
+        let thread = thread::spawn(move || {
+            loop {
+                let message = receiver.lock().unwrap().recv().unwrap();
 
-            match message {
-                Message::NewJob(job) => {
-                    println!("Worker {} got a job; executing.", id);
-                    job();
-                },
-                Message::Terminate => {
-                    println!("Worker {} was told to terminate.", id);
-                    break;
+                match message {
+                    Message::NewJob(job) => {
+                        println!("Worker {} got a job; executing.", id);
+                        job();
+                    }
+                    Message::Terminate => {
+                        println!("Worker {} was told to terminate.", id);
+                        break;
+                    }
                 }
             }
         });
 
-        Worker { id, thread: Some(thread) }
+        Worker {
+            id,
+            thread: Some(thread),
+        }
     }
 }
